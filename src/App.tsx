9b561ce3fd834c10b8fd2a114224ada5422a00f4
daniel-kav <dankavatha01@gmail.com>
2024-06-21 15:35:21 +0300

    set up my boler plate to add todo components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 // import TodoList from "./TodoList";
 import "./styles.css";
 import { useState } from "react";
+
 let initialTodos = [
-  { id: 1, todo: "Complete Online Javascript Course", completed: true },
-  { id: 2, todo: "Jog around the park 3x", completed: false },
-  { id: 3, todo: "10 minutes meditation", completed: false },
-  { id: 4, todo: "Read for 1 hour", completed: false },
-  { id: 5, todo: "Pick up grodceries", completed: false },
-  { id: 6, todo: "Complete todo app on frontend mentor", completed: false },
+  { id: 1, todo: "Buy groceries", completed: false },
+  { id: 2, todo: "Clean the house", completed: false },
+  { id: 3, todo: "Prepare dinner", completed: false },
+  { id: 4, todo: "Read a book", completed: false },
 ];
+
 export default function App() {
   const [theme, setTheme] = useState(true); // true for dark theme false for light theme
+
   function toggleHandler() {
     setTheme(!theme);
   }
+
   return (
     <div className={`${theme ? "dark" : "light"} app`}>
       <div className="background-image"></div>
-      <TodoList todos={initialTodos} onToggle={toggleHandler} />
+      {/* <TodoList todos={initialTodos} onToggle={toggleHandler} /> */}
     </div>
   );
 }
